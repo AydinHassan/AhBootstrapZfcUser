75b69283dc8bcfeaf02c1b0c2c524a266014c639
aydin <aydin@ubuntu>
2013-08-22 15:55:30 +0100

    Dependencies and config

---
+++ src/AhBootstrapZfcUser/Module.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AhBootstrapZfcUser;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+
+/**
+ * AhBootstrapZfcUser Module
+ * 
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class Module implements 
+    ConfigProviderInterface,
+    AutoloaderProviderInterface,
+    DependencyIndicatorInterface 
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+ 
+    /**
+     * {@inheritDoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getModuleDependencies() {
+        return array(
+            'ZfcUser',
+        );
+    }
+}
